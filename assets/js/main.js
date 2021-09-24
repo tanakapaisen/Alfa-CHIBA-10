0218db959d4c4f46aaf836a9e72b0700924494b2
TA-Itabashi-Takayuki <90173570+TA-Itabashi-Takayuki@users.noreply.github.com>
2021-09-24 09:21:33 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,7 +100,7 @@ app.component('answer-input', {
     return {
       /* 送信ボタン上下に表示されるメッセージ */
       okMessage: '正解！',
-      ngMessage: 'そのキーワードは違うようだぞ！？',
+      ngMessage: 'そのキdfghjkl！？',
       message: '',
       inputAnswer: '',
     }
